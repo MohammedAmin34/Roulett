c406eb91cfa652c5eee34cf88cf15242ea2d685c
MohammedAmin34 <15MA0301@amal.se>
2025-09-15 11:00:58 +0200

    Add project files.

---
+++ Roulett/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Welcome to Daddy Casino");
+
+Console.WriteLine("Please enter a number between 1-36");
+
+Console.WriteLine("How much do you whant to bet");
+
+int Money = Rand.Next(1 - 10000000);
+
+
+string input = Console.ReadLine();
+
+Random rand = new Random();
+
+int guess = Convert.ToInt32(input);
+int roulett = rand.Next(1, 36);
+Console.WriteLine(roulett);
+
+if (guess == roulett)
+{
+    Console.WriteLine("You won at Daddys Casino");
+} else
+{
+    Console.WriteLine("You lost get a life");
+}
